24df7f844fb494cc529428cf8a9835f36029f61b
Danil Shutsky <leetoplayaz@yandex.ru>
2021-09-12 21:30:53 +0300

    Update web.php
    fixed route

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/storage/{dir}/{method}/{size}/{file}', [\Leeto\Thumbnails\Controllers\ThumbnailsController::class, 'get'])
         ->where('method', 'resize|crop|fit')
         ->where('size', '\d+x\d+|\d+')
-        ->where('file', '.+\.png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG$')
+        ->where('file', '.+\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG)$')
 ->name("thumbnail");
